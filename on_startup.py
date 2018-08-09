070a5d0083cc0bc5169892e46fc53bb2897825f8
allencho <s3558745@student.rmit.edu.au>
2018-08-09 18:06:11 +1000

    Code refactoring

--- on_startup.py
+++ on_startup.py
@@ -3,6 +3,7 @@ import requests
 import json
 import time
 import os
+import accurateTemperature
  
 def send_notification_via_pushbullet(title, body):
     """ Sending notification via pushbullet.
@@ -18,53 +19,55 @@ def send_notification_via_pushbullet(title, body):
     if resp.status_code != 200:
         raise Exception('Something wrong')
     else:
-        print('complete sending')      
-def get_cpu_temp():
-    res = os.popen("vcgencmd measure_temp").readline()
-    t = float(res.replace("temp=", "").replace("'C\n", ""))
-    return(t)
+        print('complete sending')
 
-# use moving average to smooth readings
-def get_smooth(x):
-    if not hasattr(get_smooth, "t"):
-        get_smooth.t = [x, x, x]
-    get_smooth.t[2] = get_smooth.t[1]
-    get_smooth.t[1] = get_smooth.t[0]
-    get_smooth.t[0] = x
-    xs = (get_smooth.t[0]+get_smooth.t[1]+get_smooth.t[2])/3
-    return(xs)
 
-def returnAccurateTemp():
-    sense=SenseHat()
-    t1 = sense.get_temperature_from_humidity()
-    t2 = sense.get_temperature_from_pressure()
-    t_cpu = get_cpu_temp()
-    h = sense.get_humidity()
-    p = sense.get_pressure()
+# def get_cpu_temp():
+#     res = os.popen("vcgencmd measure_temp").readline()
+#     t = float(res.replace("temp=", "").replace("'C\n", ""))
+#     return(t)
 
-    # calculates the real temperature compesating CPU heating
-    t = (t1 + t2) / 2
-    t_corr = t - ((t_cpu - t) / 1.5)
-    t_corr = get_smooth(t_corr)
+# # use moving average to smooth readings
+# def get_smooth(x):
+#     if not hasattr(get_smooth, "t"):
+#         get_smooth.t = [x, x, x]
+#     get_smooth.t[2] = get_smooth.t[1]
+#     get_smooth.t[1] = get_smooth.t[0]
+#     get_smooth.t[0] = x
+#     xs = (get_smooth.t[0]+get_smooth.t[1]+get_smooth.t[2])/3
+#     return(xs)
+
+# def returnAccurateTemp():
+#     sense=SenseHat()
+#     t1 = sense.get_temperature_from_humidity()
+#     t2 = sense.get_temperature_from_pressure()
+#     t_cpu = get_cpu_temp()
+#     h = sense.get_humidity()
+#     p = sense.get_pressure()
+
+#     # calculates the real temperature compesating CPU heating
+#     t = (t1 + t2) / 2
+#     t_corr = t - ((t_cpu - t) / 1.5)
+#     t_corr = get_smooth(t_corr)
     
-    return t_corr
+#     return t_corr
 
 def main():
     
     message = "You should bring a jacket."
     sleep_time = 300  #Change to 300 (5 minutes)
     temperature_message = "It is less than 20 degrees"
-    sense = SenseHat()
+    # sense = SenseHat()
 
     cold_temp=20
     run = 0
     while run < 1:
-        t1 = sense.get_temperature_from_humidity()
-        t2 = sense.get_temperature_from_pressure()
-        t_cpu = get_cpu_temp()
+        t1 = accurateTemperature.sense.get_temperature_from_humidity()
+        t2 = accurateTemperature.sense.get_temperature_from_pressure()
+        t_cpu = accurateTemperature.get_cpu_temp()
         t = (t1 + t2) / 2
         t_corr = t - ((t_cpu - t) / 1.5)
-        t_corr = get_smooth(t_corr)
+        t_corr = accurateTemperature.get_smooth(t_corr)
         if t_corr < cold_temp:
             send_notification_via_pushbullet(temperature_message, message)
             time.sleep(sleep_time)
