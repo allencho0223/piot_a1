410c23fc82cbb3f2631745f18bc8f2c7c8a12ac9
rmit-s3604496-alex-ryland <s3604496@student.rmit.edu.au>
2018-08-17 15:25:52 +1000

    Finished greeting implementation
    Added code to greet registered bluetooth devices

--- bluetooth_greeting.py
+++ bluetooth_greeting.py
@@ -5,19 +5,28 @@ import time
 def main():
 
     try:
-        MAC_ADDRESS_FILE = open("MAC Addresses.txt", "r")
+        MAC_ADDRESS_FILE = open("/home/pi/Assignment1/MAC Addresses.txt", "r")
+        nearby_devices = bluetooth.discover_devices(lookup_names = False)
         #Infinite loop
         while True:
-            scan_file(MAC_ADDRESS_FILE)
+            scan_file(nearby_devices,MAC_ADDRESS_FILE)
     except IOError:
         print("Error: Could not open file")
 
-def scan_file(file):
+def scan_file(nearby_devices,file):
 
-    for mac in file:
-        #Remove MAC address from line read from file
-        name = mac.split(",")[0]
-        send_message(name)
+    
+    for user in file:
+        #Get mac address from user's data in txt file
+        mac = user.split(", ")[1]
+        for addresses in nearby_devices:
+
+            if addresses in mac:
+                #Remove MAC address from line read from file
+                name = user.split(",")[0]
+                send_message(name)
+
+
 
 def send_message(user_name):
 
